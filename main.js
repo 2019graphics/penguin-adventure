6346733ec167fe74ca5f6d2b1834746dca51d366
ys <lys984prms@naver.com>
2019-11-25 15:41:45 +0900

    ver1.0
    펭귄 앞으로 움직임
    빙하 랜덤 생성

--- main.js
+++ main.js
@@ -1,97 +1,134 @@
-// import * as TMJ from './three.module';
-// import { Water } from './Water2';
 var camera, scene, renderer;
 var theta = 0;
 
+//meshs
+var icebergs, penguin, seal, bear, shark, snowballs;
+var snowCount = 10;
+
+//size
+var maxX = 2000, maxY = 2000, maxZ = 1000;
+
 init();
-animate();
 
+//조합
+function randCom(total, object) {
+    //total개 중에 object개 뽑기
 
+    var lotto = new Array(object); // 6개의 배열이 lotto에 저장
+    var count = 0; //추출한 로또번호의 갯수
+    var overl = true; // 번호중복 여부 변수
+ 
+    while (count < object) { // 로또번호 6번 얻을 때까지 반복.
+        var number = 0; //랜덤번호 가져오는 변수
+        number = parseInt(Math.random() * total) + 1; // 1~45사이에 랜덤번호 추출
+ 
+        for (var i = 0; i < count; i++) { // 1부터 i까지 반복하여 중복확인
+            if (lotto[i] == number) { // 중복된 번호가 아니면 넘어가기.
+                overl = false;
+            }
+        }
+ 
+        if (overl) { //중복 없을 시 count 1 증가
+            lotto[count] = number; //추출된 번호를 배열에 넣기
+            count++;
+        }
+ 
+        overl = true; //원래 true으로 돌아가기
+    }
+    return lotto;
+}
+
+//mesh remove
 function remove(id) {
   scene.remove(scene.getObjectByName(id));
 }
 
+function meshAdd() {
+    /*iceberg*/
+    icebergs = [];
+    iceLoc = randCom(20, 10);
+    for (var i = 0; i < 10; i++) {
+        var ice = drawIce(iceLoc[i]*100, 0, 0);
+        icebergs.push(ice);
+        scene.add(ice);
+    }
+
+    /*snowball*/
+    //ver 1. create a snowball
+    /*
+    snowballs = [];
+    for (var i = 0; i < snowCount ; i++) {
+        var snowball = drawSnowBall(Math.random() * 500, Math.random() * 500, Math.random() * 500);
+        snowballs.push(snowball);
+        scene.add(snowball);
+    }
+    */
+    
+    penguin = drawPeng(0, 0, -40);
+    penguin.scale.set(0.8, 0.8, 0.8);
+    scene.add(penguin);
+    seal = drawSeal(0, 0, -0);
+    scene.add(seal);
+    bear = drawBear(0, -200, -0);
+    scene.add(bear);
+    shark = drawShark(0, -200, 200);
+    scene.add(shark);
+}
 
 function init() {
-    //ĵ���� ũ��
+    //size
     var width = window.innerWidth;
     var height = window.innerHeight;
 
-    //������
-    renderer = new THREE.WebGLRenderer({ antialias: true }); //antialias : �������� �����ڸ� ǥ�� �Ų��ϰ�
+    //render
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(width, height);
     document.body.appendChild(renderer.domElement);
 
-    //���
+    //mesh add
     scene = new THREE.Scene;
-    var penguin = drawPeng(0, 100, 150);
-    scene.add(penguin);
-    var seal = drawSeal(0, 100, -50);
-    scene.add(seal);
-    var bear = drawBear(0, -100, -50);
-    scene.add(bear);
-    var shark = drawShark(0, -100, 150);
-    scene.add(shark);
-	var ice = drawIce(0, 10, -100);
-	scene.add(ice);
-	
-	//여러개 만들어서 없애는거 못하겠우ㅠㅍㅠ
-	for (i=0;i<1 ;i++ )
-	{
-		var snowball = drawSnowBall(Math.random()*500, Math.random()*500, Math.random()*500);
-		scene.add(snowball);
-	}
+    meshAdd();
 
-    //��鿡 �޽� �߰�
-
-    //ī�޶� ����
-    //�þ�, ��Ⱦ��, ��ü�� ī�޶��� �����Ÿ�, �ִ� �Ÿ�
-    camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
-    camera.position.y = 300;
-    camera.position.z = 0;
-    camera.position.x = 300;
+    //camera
+	camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+	camera.position.set(-1000, -1000, 1000);
     camera.up.set(0, 0, 1);
     camera.lookAt(0, 0, 0);
-    scene.add(camera);//��鿡 ī�޶� �߰�
+    scene.add(camera);
 
-
-    //���� �޽�
-    var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
+    //background
+    var skyboxGeometry = new THREE.CubeGeometry(maxX, maxY, maxZ);
     var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.BackSide });
     var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+    skybox.position.set(1000, 0, 0);
+    scene.add(skybox);
 
-    scene.add(skybox);//��鿡 ��� �߰�
+    //light
+    var Light = new THREE.DirectionalLight(0xffffff,0.8);
+    //var pointLight = new THREE.PointLight(0xffffff);
+    Light.position.set(-500, -250, 350);
+    scene.add(Light);
+
+    var Light2 = new THREE.DirectionalLight(0xffffff, 0.6);
+    Light2.position.set(500, 250, 350);
+    scene.add(Light2);
+
+    var Light3 = new THREE.DirectionalLight(0xffffff, 0.1);
+    Light3.position.set(0, 0, maxZ);
+    scene.add(Light3);
 
 
-    //����
-    var pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.set(300, 200, 200);
-
-    scene.add(pointLight);
 
 
-    // var textureloader = new TMJ.TextureLoader();
-    // textureLoader.load('./ocean/textures/floors/FloorsCheckerboard_S_Diffuse.jpg', function (map) {
-
-    //     map.wrapS = THREE.RepeatWrapping;
-    //     map.wrapT = THREE.RepeatWrapping;
-    //     map.anisotropy = 16;
-    //     map.repeat.set(4, 4);
-    //     groundMaterial.map = map;
-    //     groundMaterial.needsUpdate = true;
-
-    // });
-
-  
 
 
     /* Water */
     //처음 2개까지로만 크기 조절.
-
-    var waterGeo = new THREE.PlaneGeometry(800, 2000, 50, 50);
+    var waterGeo = new THREE.PlaneGeometry(maxX, maxY, 50, 50);
     var waterMat = new THREE.MeshPhongMaterial({
         color: 0x3366CC,
         emissive: 0x009999,
+        opacity: 0.5,
         shading: THREE.FlatShading,
         shininess: 60,
         specular: 30,
@@ -104,18 +141,19 @@ function init() {
     }
 
     var waterObj = new THREE.Mesh(waterGeo, waterMat);
-    waterObj.position.z-=80
+    waterObj.position.set(1000, 0, -80);
     scene.add(waterObj);
 
 
 
-    var count = 0;
 
-    //������
     var animate = function () {
         requestAnimationFrame(animate);
+        theta += 0.01;
 
+        //wave
         var particle, i = 0;
+        var count = theta * 2;
         for (var ix = 0; ix < 50; ix++) {
             for (var iy = 0; iy < 50; iy++) {
                 waterObj.geometry.vertices[i++].z = (Math.sin((ix + count) * 2) * 3) +
@@ -123,38 +161,34 @@ function init() {
                 waterObj.geometry.verticesNeedUpdate = true;
             }
         }
-
-
-        count += 0.02;
-
         
-        theta += 0.01;
+        //snowball
+        /*
+        for (i = 0; i < snowCount ; i++) {
+            //alert(snowballs[i].position.z);
+            snowballs[i].position.z -= 3;
+            if (snowballs[i].position.z <= -200) {
+                snowballs[i].position.z = 11000;
+            }
+        }
+        */
 
-		snowball.position.z = theta*-300
-			
-
-		if (snowball.position.z <= -200)
-		{
-			scene.remove(snowball)
-		}
-		
-        //penguin.position.x = theta * 100;
-        /*var temp = (theta * 10) % 4;
-        temp = -1 * (temp - 2) * (temp - 2) + 4;
-        penguin.position.z = temp * 20;*/
+        //penguin move
+        var pengx = (theta * 200) % maxX;
+        penguin.position.x = pengx;
+        pengx %= 100;
+        var pengz = -1 * (pengx - 50) * (pengx - 50) + 2500;
+        pengz /= 50;
+        penguin.position.z = pengz;
         
 
-        //camera.position.set(400,200,200)
-        //camera.position.y = Math.cos(theta)*800;
-        //camera.position.x = Math.sin(theta)*800;
-        camera.position.y = 700;
-		camera.position.x = 1000;
-		camera.position.z = 300;
+        camera.position.set(2000, -2000, 1500);
+        //camera.position.y = Math.cos(theta)*2000;
+        //camera.position.x = Math.sin(theta)*2000;
         camera.lookAt(0, 0, 0);
 
 
         renderer.render(scene, camera);
     }
     animate();
-
 }
--- objects/ice.js
+++ objects/ice.js
@@ -1,12 +1,12 @@
 function drawIce(xOffset, yOffset, zOffset) {
-	var group = new THREE.Group();
+    var group = new THREE.Group();
+    zOffset -= 100;
 
     var geometry = new THREE.Geometry();
-	geometry.vertices.push(
-	new THREE.Vector3( -10,  10, 0 ),
-	new THREE.Vector3( -10, -10, 0 ),
-	new THREE.Vector3(  10, -10, 0 ),
-);
+    geometry.vertices.push(
+	new THREE.Vector3(-10, 10, 0),
+	new THREE.Vector3(-10, -10, 0),
+	new THREE.Vector3(10, -10, 0));
 
 	geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
 
@@ -15,49 +15,12 @@ function drawIce(xOffset, yOffset, zOffset) {
 	var geometry2 = new THREE.OctahedronGeometry(RADIUS, 1);	//숫자가 커질수록 더 동그래짐
 
 	const material = new THREE.MeshLambertMaterial({ color: 0xCCFFFF })
-	/*
-	//glacier
-	var mesh0  = new THREE.Mesh(geometry1, material)
-	mesh0.position.set(0 + xOffset, 0 + yOffset, 0 + zOffset);
-	mesh0.rotation.set(0, 10, 0);
-	group.add(mesh0);
-
-
-	var mesh1  = new THREE.Mesh(geometry1, material)
-	mesh1.position.set(0 + xOffset, 100 + yOffset, 0 + zOffset);
-	mesh1.rotation.set(0, 10, 0);
-	group.add(mesh1);
-
-	var mesh2 = new THREE.Mesh(geometry1, material)
-	mesh2.position.set(0 + xOffset, 200 + yOffset, 0 + zOffset);
-	mesh2.rotation.set(30, 20, 40);
-	group.add(mesh2);
-
-	var mesh3 = new THREE.Mesh(geometry1, material)
-	mesh3.position.set(0 + xOffset, -100 + yOffset, 0 + zOffset);
-	mesh3.rotation.set(0, 0, 0);
-	group.add(mesh3);
-
-	var mesh4 = new THREE.Mesh(geometry1, material)
-	mesh4.position.set(0 + xOffset, -200 + yOffset, 0 + zOffset);
-	mesh4.rotation.set(0, 0, 0);
-	group.add(mesh4);
-
-	for (i=1;i<10;i++)
-	{
-		var mesh4 = new THREE.Mesh(geometry1, material)
-		mesh4.position.set(0 + xOffset, -200 + yOffset+i*90, 0 + zOffset);
-		mesh4.rotation.set(Math.random()*100, 0, 0);
-		group.add(mesh4);
-	}
-*/
-
 	
 	// material texture
 	var texture = new THREE.Texture( generateTexture() );
 	texture.needsUpdate = true; // important!
 
-	for (i=0;i<14;i++)
+	for (i=0;i<20;i++)
 	{
 		//iceCube
 		var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);//geometry, 큐브 생성
