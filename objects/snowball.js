fa8364ecb8fba4b6dca52546a7837f3d79636af5
Sumin00 <laurenpsm@naver.com>
2019-11-25 12:57:51 +0900

    snow update
    add

---
+++ objects/snowball.js
@@ -0,0 +1,24 @@
+function drawSnowBall(xOffset, yOffset, zOffset) {
+	var group = new THREE.Group();
+
+    var geometry = new THREE.Geometry();
+	geometry.vertices.push(
+	new THREE.Vector3( -10,  10, 0 ),
+	new THREE.Vector3( -10, -10, 0 ),
+	new THREE.Vector3(  10, -10, 0 ),
+);
+
+	geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
+
+	var RADIUS = 10
+
+	//snowball
+	var geometry = new THREE.OctahedronGeometry(RADIUS, 4);	//숫자가 커질수록 더 동그래짐
+	const material = new THREE.MeshLambertMaterial({ color: 0xFFFFFF })
+
+	var snowball = new THREE.Mesh(geometry, material)
+	snowball.position.set(xOffset, yOffset, zOffset);
+	group.add(snowball);
+
+	return group
+}
