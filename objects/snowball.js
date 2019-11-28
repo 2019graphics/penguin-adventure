5d608e666315fd068a60998a093c50b3917c3b4f
Sumin00 <laurenpsm@naver.com>
2019-11-28 16:00:20 +0900

    score_board+add

--- objects/snowball.js
+++ objects/snowball.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function drawSnowBall(xOffset, yOffset, zOffset) {
 	var group = new THREE.Group();
  
@@ -14,7 +13,7 @@ function drawSnowBall(xOffset, yOffset, zOffset) {
 	var RADIUS = 10
  
 	//snowball
-	var geometry = new THREE.OctahedronGeometry(RADIUS, 4);   //Â¼Ã½Ã€ÃšÂ°Â¡ Ã„Â¿ÃÃºÂ¼Ã¶Â·Ã Â´Ãµ ÂµÂ¿Â±Ã—Â·Â¡ÃÃ¼
+	var geometry = new THREE.OctahedronGeometry(RADIUS, 4);
 	const material = new THREE.MeshLambertMaterial({ color: 0xFFFFFF })
  
 	var snowball = new THREE.Mesh(geometry, material)
@@ -23,29 +22,3 @@ function drawSnowBall(xOffset, yOffset, zOffset) {
  
 	return snowball
  }
-=======
-function drawSnowBall(xOffset, yOffset, zOffset) {
-	var group = new THREE.Group();
-
-    var geometry = new THREE.Geometry();
-	geometry.vertices.push(
-	new THREE.Vector3( -10,  10, 0 ),
-	new THREE.Vector3( -10, -10, 0 ),
-	new THREE.Vector3(  10, -10, 0 ),
-);
-
-	geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
-
-	var RADIUS = 10
-
-	//snowball
-	var geometry = new THREE.OctahedronGeometry(RADIUS, 4);	//¼ýÀÚ°¡ Ä¿Áú¼ö·Ï ´õ µ¿±×·¡Áü
-	const material = new THREE.MeshLambertMaterial({ color: 0xFFFFFF })
-
-	var snowball = new THREE.Mesh(geometry, material)
-	snowball.position.set(xOffset, yOffset, zOffset);
-	group.add(snowball);
-
-	return group
-}
->>>>>>> 14607a680d083891ab9bcc9193e6263795961a83
